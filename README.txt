2bc0f7d091573a93bae645c34cb13ad316fb1d28
Jo Beaver <jobeaveris@hotmail.com>
2018-02-11 10:20:22 +0000

    Display date/time in specified format

---
+++ README.txt
@@ -0,0 +1,4 @@
+A JavaScript program to display the current day and time in the following format.  
+
+Today is: Friday 
+Current time is: 4 PM : 50 : 22
---
+++ script.js
@@ -0,0 +1,21 @@
+alert("Hello world");
+
+var date = new Date();
+var dayText = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+var day = date.getDay();
+var hour = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+var ampm = pmam;
+var whichDay = dayText[day];
+
+function pmam() {
+	if (hour <= 11) {
+		ampm = "AM";
+	} else {
+		ampm = "PM";
+	}
+}
+pmam();
+
+console.log(`Today is: ${whichDay}\nCurrent time is: ${hour} ${ampm}:${min}:${sec}`);
